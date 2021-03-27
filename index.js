892c4c547ff9ad68debb6ebd9dbc35c070236178
x4slow <68307114+x4slow@users.noreply.github.com>
2021-03-27 17:37:54 -0300

    Update index.js

--- index.js
+++ index.js
@@ -7,16 +7,17 @@ client.aliases = new Discord.Collection();
 client.categories = fs.readdirSync('./commands/');
 client.prefix = config.prefix;
 
-["command", "event"].forEach(handler => {
+['command', 'event'].forEach(handler => {
     require(`./handlers/${handler}`)(client);
 });
 
   client.on('guildMemberAdd', async (member) => {
-    require("./events/guild/memberAdd")(member)
+    require('./events/guild/memberAdd')(member)
   })
 
   client.on('guildMemberRemove', async (message) => {
-    require("./events/guild/memberRemove")(message)
+    require('./events/guild/memberRemove')(message)
   })
 
+// If you use an .env file, just put the token there with process.env
 client.login(config.token);
