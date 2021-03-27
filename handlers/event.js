3bf3d258e5eb73a9316cb2b43d676c76845ee818
x4slow <68307114+x4slow@users.noreply.github.com>
2021-03-27 17:35:26 -0300

    Update event.js

--- handlers/event.js
+++ handlers/event.js
@@ -1,13 +1,13 @@
-const { readdirSync } = require("fs");
+const { readdirSync } = require('fs');
 
 module.exports = (client) => {
   const load = dirs => {
-    const events = readdirSync(`./events/${dirs}/`).filter(d => d.endsWith("js") );
+    const events = readdirSync(`./events/${dirs}/`).filter(d => d.endsWith('fs'));
     for (let file of events) {
       let evt = require(`../events/${dirs}/${file}`);
       let eName = file.split('.')[0];
-      client.on(eName, evt.bind(null,client));
+      client.on(eName, evt.bind(null, client));
     }
-  };
+  }
   ["client", "guild"].forEach((x) => load(x));
-};
+}
